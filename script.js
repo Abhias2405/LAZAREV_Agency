191ed7fe677502aff974ae50bf8e5d46499897fc
Abhias2405 <iamabhisheksingh56@gmail.com>
2024-10-13 21:47:48 +0530

    add locomotiveAnimation

--- script.js
+++ script.js
@@ -1,3 +1,40 @@
+function locomotiveAnimation() {
+    gsap.registerPlugin(ScrollTrigger);
+
+    const locoScroll = new LocomotiveScroll({
+        el: document.querySelector("#main"),
+        smooth: true,
+
+        // for tablet smooth
+        tablet: { smooth: true },
+
+        // for mobile
+        smartphone: { smooth: true }
+    });
+    locoScroll.on("scroll", ScrollTrigger.update);
+
+    ScrollTrigger.scrollerProxy("#main", {
+        scrollTop(value) {
+            return arguments.length
+                ? locoScroll.scrollTo(value, 0, 0)
+                : locoScroll.scroll.instance.scroll.y;
+        },
+        getBoundingClientRect() {
+            return {
+                top: 0,
+                left: 0,
+                width: window.innerWidth,
+                height: window.innerHeight
+            };
+        }
+    });
+
+    ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+    ScrollTrigger.refresh();
+}
+
+
 function navAnimation() {
     var nav = document.querySelector("nav")
 
@@ -107,7 +144,7 @@ function page3VideoAnimation() {
 
 }
 
-
+locomotiveAnimation()
 navAnimation()
 page2Animation()
 page3VideoAnimation()
